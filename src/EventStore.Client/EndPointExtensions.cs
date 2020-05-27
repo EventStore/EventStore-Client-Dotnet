f99fc41de8e412baa68a71d073c3311ab4c425a4
shaan1337 <sniper111@gmail.com>
2020-05-27 14:20:10 +0400

    Do gossip requests over gRPC Update VNodeState enum to match server and add unused values to notAllowedStates

--- src/EventStore.Client/EndPointExtensions.cs
+++ src/EventStore.Client/EndPointExtensions.cs
@@ -3,6 +3,9 @@ using System.Net;
 
 namespace EventStore.Client {
 	static class EndPointExtensions {
+		public static string HTTP_SCHEMA => Uri.UriSchemeHttp;
+		public static string HTTPS_SCHEMA => Uri.UriSchemeHttps;
+
 		public static string GetHost(this EndPoint endpoint) =>
 			endpoint switch {
 				IPEndPoint ip => ip.Address.ToString(),
@@ -18,5 +21,18 @@ namespace EventStore.Client {
 				_ => throw new ArgumentOutOfRangeException(nameof(endpoint), endpoint?.GetType(),
 					"An invalid endpoint has been provided")
 			};
+
+		public static string ToHttpUrl(this EndPoint endPoint, string schema, string rawUrl = null) =>
+			endPoint switch {
+				IPEndPoint ipEndPoint => CreateHttpUrl(schema, ipEndPoint.Address.ToString(), ipEndPoint.Port,
+					rawUrl != null ? rawUrl.TrimStart('/') : string.Empty),
+				DnsEndPoint dnsEndpoint => CreateHttpUrl(schema, dnsEndpoint.Host, dnsEndpoint.Port,
+					rawUrl != null ? rawUrl.TrimStart('/') : string.Empty),
+				_ => null
+			};
+
+		private static string CreateHttpUrl(string schema, string host, int port, string path) {
+			return $"{schema}://{host}:{port}/{path}";
+		}
 	}
 }
