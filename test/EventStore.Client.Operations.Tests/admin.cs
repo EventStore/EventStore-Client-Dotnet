27a14452fc8980dae8a63beb92d3e747935d39f8
Pieter Germishuys <pieter@pieterg.com>
2020-08-05 12:07:25 +0200

    Move disruptive node tests into separate test fixtures

--- test/EventStore.Client.Operations.Tests/admin.cs
+++ test/EventStore.Client.Operations.Tests/admin.cs
@@ -10,36 +10,6 @@ namespace EventStore.Client {
 		}
 
 		[Fact]
-		public async Task shutdown_does_not_throw() {
-			await _fixture.Client.ShutdownAsync(userCredentials: TestCredentials.Root);
-		}
-
-		[Fact]
-		public async Task shutdown_without_credentials_throws() {
-			await Assert.ThrowsAsync<AccessDeniedException>(() => _fixture.Client.ShutdownAsync());
-		}
-
-		[Fact]
-		public async Task set_node_priority_does_not_throw() {
-			await _fixture.Client.SetNodePriorityAsync(1000, TestCredentials.Root);
-		}
-
-		[Fact]
-		public async Task set_node_priority_without_credentials_throws() {
-			await Assert.ThrowsAsync<AccessDeniedException>(() => _fixture.Client.SetNodePriorityAsync(1000));
-		}
-
-		[Fact]
-		public async Task resign_node_does_not_throw() {
-			await _fixture.Client.ResignNodeAsync(TestCredentials.Root);
-		}
-
-		[Fact]
-		public async Task resign_node_without_credentials_throws() {
-			await Assert.ThrowsAsync<AccessDeniedException>(() => _fixture.Client.ResignNodeAsync());
-		}
-
-		[Fact]
 		public async Task merge_indexes_does_not_throw() {
 			await _fixture.Client.MergeIndexesAsync(TestCredentials.Root);
 		}
---
+++ test/EventStore.Client.Operations.Tests/admin_resign_node.cs
@@ -0,0 +1,27 @@
+using System.Threading.Tasks;
+using Xunit;
+
+namespace EventStore.Client {
+	public class admin_resign_node : IClassFixture<admin_resign_node.Fixture> {
+		private readonly Fixture _fixture;
+
+		public admin_resign_node(Fixture fixture) {
+			_fixture = fixture;
+		}
+
+		[Fact]
+		public async Task resign_node_does_not_throw() {
+			await _fixture.Client.ResignNodeAsync(TestCredentials.Root);
+		}
+
+		[Fact]
+		public async Task resign_node_without_credentials_throws() {
+			await Assert.ThrowsAsync<AccessDeniedException>(() => _fixture.Client.ResignNodeAsync());
+		}
+		
+		public class Fixture : EventStoreClientFixture {
+			protected override Task Given() => Task.CompletedTask;
+			protected override Task When() => Task.CompletedTask;
+		}
+	}
+}
