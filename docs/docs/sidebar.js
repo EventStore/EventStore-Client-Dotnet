ac375b3693ea1e0b914652152a5d697ef75a6999
Alexey Zimarev <alex@zimarev.com>
2020-10-28 18:08:53 +0100

    Fixing things

--- docs/docs/sidebar.js
+++ docs/docs/sidebar.js
@@ -1,42 +1,43 @@
-module.exports =[
+module.exports = [
 	{
 		title: "Getting started",
 		collapsable: true,
 		children: [
-			"getting-started/introduction",
+			"getting-started/",
 			"getting-started/installation",
 			"getting-started/quick-tour"
 		]
 	},
+	// {
+	// 	title: "Changelog",
+	// 	collapsable: false
+	// },
 	{
-		title: "Changelog",
-		collapsable: false
-	},
-	{
-		title: "Connecting to Event Store",
+		title: "Connecting to EventStoreDB",
 		collapsable: true,
 		children: [
-			"connecting-to-eventstore/connecting-to-a-single-node",
-			"connecting-to-eventstore/connecting-to-a-cluster",
-			"connecting-to-eventstore/connecting-to-eventstore-cloud",
-			"connecting-to-eventstore/using-a-custom-httpmessagehandler",
-			"connecting-to-eventstore/adding-a-grpc-interceptor",
-			"connecting-to-eventstore/tls",
-			"connecting-to-eventstore/setting-up-a-connection-with-net-dependency-injection"
+			"connecting/",
+			"connecting/single-node",
+			"connecting/cluster",
+			"connecting/es-cloud",
+			"connecting/custom-httpmessagehandler",
+			"connecting/grpc-interceptor",
+			// "connecting/tls",
+			"connecting/di-extensions"
 		]
 	},
 	{
 		title: "Authentication",
 		collapsable: true,
 		children: [
-			"authentication/authenticating-with-username-password"
+			"authentication/"
 		]
 	},
 	{
 		title: "Writing events",
 		collapsable: true,
 		children: [
-			"writing-events/basics",
+			"writing-events/",
 			"writing-events/event-versioning-strategies"
 		]
 	},
@@ -44,7 +45,7 @@ module.exports =[
 		title: "Reading events",
 		collapsable: true,
 		children: [
-			"reading-events/basics",
+			"reading-events/",
 			"reading-events/reading-from-a-stream",
 			"reading-events/reading-from-the-all-stream"
 		]
@@ -53,37 +54,37 @@ module.exports =[
 		title: "Subscribing to streams",
 		collapsable: true,
 		children: [
-			"subscribing-to-streams/basics",
+			"subscribing-to-streams/",
 			"subscribing-to-streams/filtering",
-			"subscribing-to-streams/error-handling"
+			// "subscribing-to-streams/error-handling"
 		]
 	},
-	{
-		title: "Projections",
-		collapsable: true,
-		children: [
-			"projections/creating-a-projection"
-		]
-	},
-	{
-		title: "Persistent subscriptions",
-		collapsable: true,
-		children: [
-			"persistent-subscriptions/creating-a-persistent-subscription",
-			"persistent-subscriptions/subscribing-to-persistent-subscription",
-			"persistent-subscriptions/advanced"
-		]
-	},
-	{
-		title: "Examples",
-		collapsable: false,
-	},
-	{
-		title: "Source code",
-		collapsable: false,
-	},
-	{
-		title: "Issues and help",
-		collapsable: true,
-	}
+	// {
+	// 	title: "Projections",
+	// 	collapsable: true,
+	// 	children: [
+	// 		"projections/creating-a-projection"
+	// 	]
+	// },
+	// {
+	// 	title: "Persistent subscriptions",
+	// 	collapsable: true,
+	// 	children: [
+	// 		"persistent-subscriptions/creating-a-persistent-subscription",
+	// 		"persistent-subscriptions/subscribing-to-persistent-subscription",
+	// 		"persistent-subscriptions/advanced"
+	// 	]
+	// },
+	// {
+	// 	title: "Examples",
+	// 	collapsable: false,
+	// },
+	// {
+	// 	title: "Source code",
+	// 	collapsable: false,
+	// },
+	// {
+	// 	title: "Issues and help",
+	// 	collapsable: true,
+	// }
 ]
--- docs/docs/subscribing-to-streams/filtering.md
+++ docs/docs/subscribing-to-streams/filtering.md
@@ -4,17 +4,17 @@ EventStoreDB allows you to filter the events whilst you subscribe to the `$all` 
 
 You can filter by event type or stream name using either a regular expression or a prefix. Server-side filtering is currently only available on the `$all` stream.
 
-:::tip
+::: tip
 Server-side filtering introduced as a simpler alternative to projections. Before creating a projection to get the events you care about you should first consider filtering.
 :::
 
 ## Filtering out system events
 
-There are a number of events in EventStoreDB called system events. These are prefixed with a `$` and under most circumstances you won't care about these. The can befiltered out by passing in a `SubscriptionFilterOptions` when subscribing to the all stream.
+There are a number of events in EventStoreDB called system events. These are prefixed with a `$` and under most circumstances you won't care about these. They can be filtered out by passing in a `SubscriptionFilterOptions` when subscribing to the `$all` stream.
 
 <<< @/samples/server-side-filtering/Program.cs#exclude-system
 
-:::tip
+::: tip
 `$stats` events are no longer stored in EventStoreDB by default so there won't be as many `$` events as before.
 :::
 
@@ -59,7 +59,8 @@ If you want to subscribe to multiple streams then it might be better to provide 
 This will subscribe to any stream with a name that begins with `account` or `savings`.
 
 ## Checkpointing
-There is one thing to consider with server-side filtering, and that is when events that match your filter are few and far between. In this scenario, you might find yourself in the situation where EventStoreDB has searched through 1 million events and the last thing you want to happen is for the server to get to event 900k and then have your client crash. It won't have been able to take a checkpoint and upon restart, you'd have to go back to the beginning and start again.
+
+There is one thing to consider with server-side filtering, and that is when events that match your filter are few and far between. In this scenario, you might find yourself in the situation where EventStoreDB has searched through 1 million events, and the last thing you want to happen is for the server to get to event 900k and then have your client crash. It won't have been able to take a checkpoint and upon a restart, you'd have to go back to the beginning and start again.
 
 In this case you can make use of an additional delegate that will be triggered every n number of events (32 by default).
 
@@ -67,10 +68,10 @@ To make use of it set up `checkpointReached` on the `SubscriptionFilterOptions` 
 
 <<< @/samples/server-side-filtering/Program.cs#checkpoint
 
- This will be called every n number of events. If you want to be specific about the number of events threshold you can also pass that in.
+ This will be called every `n` number of events. If you want to be specific about the number of events threshold you can also pass that as a parameter.
 
 <<< @/samples/server-side-filtering/Program.cs#checkpoint-with-interval
 
-:::warning
-This number will be called every n * 32 events.
+::: warning
+This number will be called every `n * 32` events.
 :::
