ebfa7eb93f9dee4557faca60b433c7596ae17345
thefringeninja <495495+thefringeninja@users.noreply.github.com>
2021-01-20 12:49:03 +0100

    check disposal before invoking checkpointReached

---
+++ test/EventStore.Client.Streams.Tests/Bugs/Issue_104.cs
@@ -0,0 +1,55 @@
+using System.Threading.Tasks;
+using Xunit;
+
+namespace EventStore.Client.Bugs {
+	public class Issue_104 : IClassFixture<Issue_104.Fixture> {
+		private readonly Fixture _fixture;
+
+		public Issue_104(Fixture fixture) {
+			_fixture = fixture;
+		}
+
+		[Fact]
+		public async Task subscription_does_not_send_checkpoint_reached_after_disposal() {
+			var streamName = _fixture.GetStreamName();
+			var ignoredStreamName = $"ignore_{streamName}";
+			var subscriptionDisposed = new TaskCompletionSource<bool>();
+			var eventAppeared = new TaskCompletionSource<bool>();
+			var checkpointReachAfterDisposed = new TaskCompletionSource<bool>();
+
+			await _fixture.Client.AppendToStreamAsync(streamName, StreamRevision.None, _fixture.CreateTestEvents());
+
+			var subscription = await _fixture.Client.SubscribeToAllAsync((_, __, ___) => {
+					eventAppeared.TrySetResult(true);
+					return Task.CompletedTask;
+				}, false,
+				(_, __, ____) => subscriptionDisposed.TrySetResult(true),
+				new SubscriptionFilterOptions(StreamFilter.Prefix(streamName), 1, (_, __, ____) => {
+					if (!subscriptionDisposed.Task.IsCompleted) {
+						return Task.CompletedTask;
+					}
+
+					checkpointReachAfterDisposed.TrySetResult(true);
+					return Task.CompletedTask;
+				}), userCredentials: new UserCredentials("admin", "changeit"));
+
+			await eventAppeared.Task;
+
+			subscription.Dispose();
+			await subscriptionDisposed.Task;
+
+			await _fixture.Client.AppendToStreamAsync(ignoredStreamName, StreamRevision.None,
+				_fixture.CreateTestEvents(50));
+
+			var delay = Task.Delay(300);
+			var result = await Task.WhenAny(delay, checkpointReachAfterDisposed.Task);
+			Assert.Equal(delay, result); // iow 300ms have passed without seeing checkpointReachAfterDisposed
+		}
+
+		public class Fixture : EventStoreClientFixture {
+			protected override Task Given() => Task.CompletedTask;
+
+			protected override Task When() => Task.CompletedTask;
+		}
+	}
+}
