7634b875d30e576d339c19be17025060af744df7
Oskar Dudycz <oskar.dudycz@gmail.com>
2021-01-26 15:33:16 +0100

    Updated TLS Sample description and comments to illustrate assumptions better

--- samples/secure-with-tls/create-certs.ps1
+++ samples/secure-with-tls/create-certs.ps1
@@ -1,3 +1,5 @@
+New-Item -ItemType Directory -Force -Path certs
+
 docker-compose -f docker-compose.generate-certs.yml up
 
 Import-Certificate -FilePath ".\certs\ca\ca.crt" -CertStoreLocation Cert:\CurrentUser\Root
--- samples/secure-with-tls/create-certs.sh
+++ samples/secure-with-tls/create-certs.sh
@@ -1,1 +1,2 @@
+mkdir -p certs
 docker-compose -f docker-compose.generate-certs.yml up
