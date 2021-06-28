3395f4ea38a9b20351716e0eee2ceebc7372991e
thefringeninja <495495+thefringeninja@users.noreply.github.com>
2021-06-28 14:20:05 +0200

    add additional valid projection states to test (disable)

--- test/EventStore.Client.ProjectionManagement.Tests/disable.cs
+++ test/EventStore.Client.ProjectionManagement.Tests/disable.cs
@@ -11,11 +11,12 @@ namespace EventStore.Client {
 		}
 
 		[Fact]
-		public async Task status_is_stopped() {
+		public async Task status_is_aborted_stopped_abortedstopped() {
 			var name = StandardProjections.Names.First();
 			await _fixture.Client.DisableAsync(name, TestCredentials.Root);
 			var result = await _fixture.Client.GetStatusAsync(name, TestCredentials.Root);
-			Assert.Equal("Stopped", result.Status);
+			Assert.Contains(result.Status, new[] {"Aborted/Stopped", "Aborted", "Stopped"});
+
 		}
 
 		public class Fixture : EventStoreClientFixture {
