c92a271573cbc80ed8068d94a0288b2949c96ed6
thefringeninja <495495+thefringeninja@users.noreply.github.com>
2021-11-29 11:34:51 +0100

    regression test for checkpoint reached on filtered subscriptions

--- test/EventStore.Client.Streams.Tests/subscribe_to_all_filtered_live.cs
+++ test/EventStore.Client.Streams.Tests/subscribe_to_all_filtered_live.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using EventStore.Client.Streams;
-using Serilog;
 using Xunit;
 using Xunit.Abstractions;
 
