518968544532f6582002601135a8b9aa7be9ad6b
Sérgio Silveira <sdsilveira@gmail.com>
2023-10-31 16:45:05 +0100

    * quickly added some assembly info files to disable parallel test... * removed zombie code

--- src/EventStore.Client/UserCredentials.cs
+++ src/EventStore.Client/UserCredentials.cs
@@ -1,72 +1,8 @@
-using System;
 using System.Net.Http.Headers;
 using System.Text;
 using static System.Convert;
 
 namespace EventStore.Client {
-    // /// <summary>
-    // /// Represents either a username/password pair or a JWT token used for authentication and
-    // /// authorization to perform operations on the EventStoreDB.
-    // /// </summary>
-    // public class UserCredentials2 {
-    //        // ReSharper disable once InconsistentNaming
-    //        static readonly UTF8Encoding UTF8NoBom = new UTF8Encoding(false);
-    //        
-    // 	/// <summary>
-    // 	/// The username
-    // 	/// </summary>
-    // 	public string? Username => TryGetBasicAuth(0, out var value) ? value : null;
-    // 	/// <summary>
-    // 	/// The password
-    // 	/// </summary>
-    // 	public string? Password => TryGetBasicAuth(1, out var value) ? value : null;
-    //
-    // 	private readonly AuthenticationHeaderValue _authorization;
-    //
-    // 	/// <summary>
-    // 	/// Constructs a new <see cref="UserCredentials"/>.
-    // 	/// </summary>
-    // 	/// <param name="username"></param>
-    // 	/// <param name="password"></param>
-    //        public UserCredentials(string username, string password) : this(
-    //            new AuthenticationHeaderValue(
-    //                Constants.Headers.BasicScheme,
-    //                ToBase64String(UTF8NoBom.GetBytes($"{username}:{password}"))
-    //            )
-    //        ) { }
-    //
-    // 	/// <summary>
-    // 	/// Constructs a new <see cref="UserCredentials"/>.
-    // 	/// </summary>
-    // 	/// <param name="authToken"></param>
-    // 	public UserCredentials(string authToken) : this(new AuthenticationHeaderValue(Constants.Headers.BearerScheme, authToken)) { }
-    //
-    // 	private UserCredentials(AuthenticationHeaderValue authorization) => _authorization = authorization;
-    //
-    // 	private bool TryGetBasicAuth(int index, out string? value) {
-    // 		value = null;
-    //
-    // 		if (_authorization.Scheme != Constants.Headers.BasicScheme) {
-    // 			return false;
-    // 		}
-    //
-    // 		if (_authorization.Parameter == null) {
-    // 			return false;
-    // 		}
-    //
-    // 		var parts = UTF8NoBom.GetString(FromBase64String(_authorization.Parameter)).Split(':');
-    // 		if (parts.Length <= index) {
-    // 			return false;
-    // 		}
-    //
-    // 		value = parts[index];
-    // 		return true;
-    // 	}
-    //
-    // 	/// <inheritdoc />
-    // 	public override string ToString() => _authorization.ToString();
-    // }
-
     /// <summary>
     /// Represents either a username/password pair or a JWT token used for authentication and
     /// authorization to perform operations on the EventStoreDB.
