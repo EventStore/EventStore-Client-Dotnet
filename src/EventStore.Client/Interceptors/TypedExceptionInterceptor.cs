2dd50e56caed0abe34e06887dfdd336d5ce19724
Sérgio Silveira <sdsilveira@gmail.com>
2023-12-08 12:24:28 +0100

    more re-org stuff killed zombie code

--- src/EventStore.Client/Interceptors/TypedExceptionInterceptor.cs
+++ src/EventStore.Client/Interceptors/TypedExceptionInterceptor.cs
@@ -99,23 +99,23 @@ static class RpcExceptionConversionExtensions {
 	public static IAsyncStreamReader<TRequest> Apply<TRequest>(this IAsyncStreamReader<TRequest> reader, Func<RpcException, Exception> convertException) => 
 		new ExceptionConverterStreamReader<TRequest>(reader, convertException);
 
-	// public static Task<TResponse> Apply<TResponse>(this Task<TResponse> task, Func<RpcException, Exception> convertException) => 
-	// 	task.ContinueWith(t => t.Exception?.InnerException is RpcException ex ? throw convertException(ex) : t.Result);
+	public static Task<TResponse> Apply<TResponse>(this Task<TResponse> task, Func<RpcException, Exception> convertException) => 
+		task.ContinueWith(t => t.Exception?.InnerException is RpcException ex ? throw convertException(ex) : t.Result);
+	
+	// public static async Task<TResponse> Apply<TResponse>(this Task<TResponse> task, Func<RpcException, Exception> convertException) {
+	// 	try {
+	// 		return await task.ConfigureAwait(false);
+	// 	}
+	// 	catch (Exception ex) {
+	// 		if (ex is RpcException rpcex)
+	// 			throw convertException(rpcex);
 	//
-	public static async Task<TResponse> Apply<TResponse>(this Task<TResponse> task, Func<RpcException, Exception> convertException) {
-		try {
-			return await task.ConfigureAwait(false);
-		}
-		catch (Exception ex) {
-			if (ex is RpcException rpcex)
-				throw convertException(rpcex);
-
-			if(ex is not OperationCanceledException)
-				throw new Exception($"WTF!? {ex.Message}", ex);
-
-			throw;
-		}
-	}
+	// 		if(ex is not OperationCanceledException)
+	// 			throw new Exception($"WTF!? {ex.Message}", ex);
+	//
+	// 		throw;
+	// 	}
+	// }
 	
 	public static AccessDeniedException ToAccessDeniedException(this RpcException exception) => 
 		new(exception.Message, exception);
@@ -155,122 +155,3 @@ class ExceptionConverterStreamReader<TResponse>(IAsyncStreamReader<TResponse> re
 		}
 	}
 }
-
-
-internal class TypedExceptionInterceptorOld : Interceptor {
-	private static readonly IDictionary<string, Func<RpcException, Exception>> DefaultExceptionMap =
-		new Dictionary<string, Func<RpcException, Exception>> {
-			[Constants.Exceptions.AccessDenied] = ex => new AccessDeniedException(ex.Message, ex),
-			[Constants.Exceptions.NotLeader] = ex => new NotLeaderException(
-					ex.Trailers.FirstOrDefault(x => x.Key == Constants.Exceptions.LeaderEndpointHost)?.Value!,
-					ex.Trailers.GetIntValueOrDefault(Constants.Exceptions.LeaderEndpointPort), ex)
-		};
-
-	private readonly IDictionary<string, Func<RpcException, Exception>> _exceptionMap;
-
-	public TypedExceptionInterceptorOld(IDictionary<string, Func<RpcException, Exception>> exceptionMap) {
-		_exceptionMap = new Dictionary<string, Func<RpcException, Exception>>(DefaultExceptionMap);
-		foreach (var pair in exceptionMap) {
-			_exceptionMap.Add(pair);
-		}
-	}
-
-	public override AsyncServerStreamingCall<TResponse> AsyncServerStreamingCall<TRequest, TResponse>(
-		TRequest request,
-		ClientInterceptorContext<TRequest, TResponse> context,
-		AsyncServerStreamingCallContinuation<TRequest, TResponse> continuation) {
-		var response = continuation(request, context);
-
-		return new AsyncServerStreamingCall<TResponse>(
-			new AsyncStreamReader<TResponse>(_exceptionMap, response.ResponseStream),
-			response.ResponseHeadersAsync, response.GetStatus, response.GetTrailers, response.Dispose);
-	}
-
-	public override AsyncClientStreamingCall<TRequest, TResponse> AsyncClientStreamingCall<TRequest, TResponse>(
-		ClientInterceptorContext<TRequest, TResponse> context,
-		AsyncClientStreamingCallContinuation<TRequest, TResponse> continuation) {
-		var response = continuation(context);
-
-		return new AsyncClientStreamingCall<TRequest, TResponse>(
-			response.RequestStream,
-			response.ResponseAsync.ContinueWith(t => t.Exception?.InnerException is RpcException ex
-				? throw ConvertRpcException(ex, _exceptionMap)
-				: t.Result),
-			response.ResponseHeadersAsync,
-			response.GetStatus,
-			response.GetTrailers,
-			response.Dispose);
-	}
-
-	public override AsyncUnaryCall<TResponse> AsyncUnaryCall<TRequest, TResponse>(
-		TRequest request,
-		ClientInterceptorContext<TRequest, TResponse> context,
-		AsyncUnaryCallContinuation<TRequest, TResponse> continuation) {
-		var response = continuation(request, context);
-
-		return new AsyncUnaryCall<TResponse>(
-			response.ResponseAsync.ContinueWith(
-				t =>
-					t.Exception?.InnerException is RpcException ex
-						? throw ConvertRpcException(ex, _exceptionMap)
-						: t.Result
-			),
-			response.ResponseHeadersAsync,
-			response.GetStatus,
-			response.GetTrailers,
-			response.Dispose
-		);
-	}
-
-	public override AsyncDuplexStreamingCall<TRequest, TResponse> AsyncDuplexStreamingCall<TRequest, TResponse>(
-		ClientInterceptorContext<TRequest, TResponse> context,
-		AsyncDuplexStreamingCallContinuation<TRequest, TResponse> continuation) {
-		var response = continuation(context);
-
-		return new AsyncDuplexStreamingCall<TRequest, TResponse>(
-			response.RequestStream,
-			new AsyncStreamReader<TResponse>(_exceptionMap, response.ResponseStream),
-			response.ResponseHeadersAsync,
-			response.GetStatus,
-			response.GetTrailers,
-			response.Dispose);
-	}
-
-	private static Exception ConvertRpcException(RpcException ex,
-		IDictionary<string, Func<RpcException, Exception>> exceptionMap) {
-		Func<RpcException, Exception>? factory = null;
-		return (ex.Trailers.TryGetValue(Constants.Exceptions.ExceptionKey, out var key) &&
-		        exceptionMap.TryGetValue(key!, out factory)) switch {
-			true => factory!.Invoke(ex),
-			false => (ex.StatusCode, ex.Status.Detail) switch {
-				(StatusCode.Unavailable, "Deadline Exceeded") => new RpcException(new Status(
-					StatusCode.DeadlineExceeded, ex.Status.Detail, ex.Status.DebugException)),
-				(StatusCode.DeadlineExceeded, _) => ex,
-				(StatusCode.Unauthenticated, _) => new NotAuthenticatedException(ex.Message, ex),
-				_ => ex
-			}
-		};
-	}
-
-	private class AsyncStreamReader<TResponse> : IAsyncStreamReader<TResponse> {
-		private readonly IDictionary<string, Func<RpcException, Exception>> _exceptionMap;
-		private readonly IAsyncStreamReader<TResponse> _inner;
-
-		public AsyncStreamReader(IDictionary<string, Func<RpcException, Exception>> exceptionMap,
-			IAsyncStreamReader<TResponse> inner) {
-			_exceptionMap = exceptionMap;
-			_inner = inner;
-		}
-
-		public async Task<bool> MoveNext(CancellationToken cancellationToken) {
-			try {
-				return await _inner.MoveNext(cancellationToken).ConfigureAwait(false);
-			} catch (RpcException ex) {
-				throw ConvertRpcException(ex, _exceptionMap);
-			}
-		}
-
-		public TResponse Current => _inner.Current;
-	}
-}
-
