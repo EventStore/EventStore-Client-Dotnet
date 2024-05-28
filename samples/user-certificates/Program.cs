015b84551677db174a8699131901f9034e4710e8
William Chong <william-chong@outlook.com>
2024-05-28 13:03:55 +0400

    Add OTel and User certificates samples (#307)

---
+++ samples/user-certificates/Program.cs
@@ -0,0 +1,22 @@
+﻿await ClientWithUserCertificates();
+
+return;
+
+static async Task ClientWithUserCertificates() {
+	try {
+		# region client-with-user-certificates
+
+		const string userCertFile = "/path/to/user.crt";
+		const string userKeyFile  = "/path/to/user.key";
+
+		var settings = EventStoreClientSettings.Create(
+			$"esdb://localhost:2113/?tls=true&tlsVerifyCert=true&userCertFile={userCertFile}&userKeyFile={userKeyFile}"
+		);
+
+		await using var client = new EventStoreClient(settings);
+
+		# endregion client-with-user-certificates
+	} catch (InvalidClientCertificateException) {
+		// ignore for sample purposes
+	}
+}
