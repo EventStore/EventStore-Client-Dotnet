2cdf1f002a1d23532bd12ffe4cbfd5b96db3658e
Sérgio Silveira <sdsilveira@gmail.com>
2024-12-18 17:07:03 +0100

    moved files into main project and deleted remaining empty projects dropped support for net6.0 and net7.0 added some helpers to fluentdocker playing around the idea of keeping the container on tests

--- test/EventStore.Client.Tests.Common/FluentDocker/FluentDockerBuilderExtensions.cs
+++ test/EventStore.Client.Tests.Common/FluentDocker/FluentDockerBuilderExtensions.cs
@@ -1,8 +1,12 @@
 using System.Reflection;
 using Ductus.FluentDocker.Builders;
+using Ductus.FluentDocker.Common;
 using Ductus.FluentDocker.Model.Compose;
 using Ductus.FluentDocker.Services;
 using Ductus.FluentDocker.Services.Impl;
+using Polly;
+using Polly.Contrib.WaitAndRetry;
+using static System.TimeSpan;
 
 namespace EventStore.Client.Tests.FluentDocker;
 
@@ -21,4 +25,64 @@ public static class FluentDockerBuilderExtensions {
 		(DockerComposeConfig)typeof(DockerComposeCompositeService)
 			.GetProperty("Config", BindingFlags.NonPublic | BindingFlags.Instance)!
 			.GetValue(service)!;
+}
+
+[PublicAPI]
+public static class FluentDockerContainerBuilderExtensions {
+    public static ContainerBuilder WithEnvironment(this ContainerBuilder builder, Dictionary<string, string?> environment) =>
+        builder.WithEnvironment(environment.Select(pair => $"{pair.Key}={pair.Value}").ToArray());
+
+    public static ContainerBuilder WaitUntilReady(this ContainerBuilder builder, IEnumerable<TimeSpan> sleepDurations, Action<IContainerService> action) =>
+        builder.Wait("", (service, _) => {
+            var result = Policy
+                .Handle<Exception>()
+                .WaitAndRetry(sleepDurations)
+                .ExecuteAndCapture(() => action(service));
+
+            if (result.Outcome == OutcomeType.Successful)
+                return 0;
+
+            throw result.FinalException is FluentDockerException
+                ? result.FinalException
+                : new FluentDockerException($"Service {service.Name} not ready: {result.FinalException.Message}");
+        });
+
+    public static ContainerBuilder WaitUntilReadyWithConstantBackoff(
+        this ContainerBuilder builder, TimeSpan delay, int retryCount, Action<IContainerService> action
+    ) => builder.WaitUntilReady(Backoff.ConstantBackoff(delay, retryCount), action);
+
+    public static ContainerBuilder WaitUntilReadyWithExponentialBackoff(
+        this ContainerBuilder builder, TimeSpan delay, int retryCount, Action<IContainerService> action
+    ) => builder.WaitUntilReady(Backoff.ExponentialBackoff(delay, retryCount), action);
+
+    public static ContainerBuilder WaitUntilReadyWithConstantBackoff(
+        this ContainerBuilder builder, int delayMs, int retryCount, Action<IContainerService> action
+    ) => builder.WaitUntilReadyWithConstantBackoff(FromMilliseconds(delayMs), retryCount, action);
+
+    public static ContainerBuilder WaitUntilReadyWithExponentialBackoff(
+        this ContainerBuilder builder, int delayMs, int retryCount, Action<IContainerService> action
+    ) => builder.WaitUntilReadyWithExponentialBackoff(FromMilliseconds(delayMs), retryCount, action);
+
+    public static ContainerBuilder WaitUntilReadyAsync(this ContainerBuilder builder, IEnumerable<TimeSpan> sleepDurations, Func<IContainerService, ValueTask> action) =>
+        builder.WaitUntilReady(sleepDurations, service => {
+            var valueTask = action(service);
+            if (!valueTask.IsCompletedSuccessfully)
+                valueTask.AsTask().GetAwaiter().GetResult();
+        });
+
+    public static ContainerBuilder WaitUntilReadyWithConstantBackoffAsync(
+        this ContainerBuilder builder, TimeSpan delay, int retryCount, Func<IContainerService, ValueTask> action
+    ) => builder.WaitUntilReadyAsync(Backoff.ConstantBackoff(delay, retryCount, fastFirst: true), action);
+
+    public static ContainerBuilder WaitUntilReadyWithExponentialBackoffAsync(
+        this ContainerBuilder builder, TimeSpan delay, int retryCount, Func<IContainerService, ValueTask> action
+    ) => builder.WaitUntilReadyAsync(Backoff.ExponentialBackoff(delay, retryCount, fastFirst: true), action);
+
+    public static ContainerBuilder WaitUntilReadyWithConstantBackoffAsync(
+        this ContainerBuilder builder, int delayMs, int retryCount, Func<IContainerService, ValueTask> action
+    ) => builder.WaitUntilReadyWithConstantBackoffAsync(FromMilliseconds(delayMs), retryCount, action);
+
+    public static ContainerBuilder WaitUntilReadyWithExponentialBackoffAsync(
+        this ContainerBuilder builder, int delayMs, int retryCount, Func<IContainerService, ValueTask> action
+    ) => builder.WaitUntilReadyWithExponentialBackoffAsync(FromMilliseconds(delayMs), retryCount, action);
 }
--- test/EventStore.Client.Tests/GrpcServerCapabilitiesClientTests.cs
+++ test/EventStore.Client.Tests/GrpcServerCapabilitiesClientTests.cs
@@ -1,102 +1,102 @@
-#if NET
-using System.Net;
-using EventStore.Client.ServerFeatures;
-using Grpc.Core;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.TestHost;
-using Microsoft.Extensions.DependencyInjection;
-
-namespace EventStore.Client.Tests;
-
-public class GrpcServerCapabilitiesClientTests {
-	public static IEnumerable<object?[]> ExpectedResultsCases() {
-		yield return new object?[] { new SupportedMethods(), new ServerCapabilities() };
-		yield return new object?[] {
-			new SupportedMethods {
-				Methods = {
-					new SupportedMethod {
-						ServiceName = "event_store.client.streams.streams",
-						MethodName  = "batchappend"
-					}
-				}
-			},
-			new ServerCapabilities(true)
-		};
-
-		yield return new object?[] {
-			new SupportedMethods {
-				Methods = {
-					new SupportedMethod {
-						ServiceName = "event_store.client.persistent_subscriptions.persistentsubscriptions",
-						MethodName  = "read",
-						Features = {
-							"all"
-						}
-					}
-				}
-			},
-			new ServerCapabilities(SupportsPersistentSubscriptionsToAll: true)
-		};
-
-		yield return new object?[] {
-			new SupportedMethods {
-				Methods = {
-					new SupportedMethod {
-						ServiceName = "event_store.client.persistent_subscriptions.persistentsubscriptions",
-						MethodName  = "read"
-					}
-				}
-			},
-			new ServerCapabilities()
-		};
-	}
-
-	[Theory]
-	[MemberData(nameof(ExpectedResultsCases))]
-	internal async Task GetAsyncReturnsExpectedResults(
-		SupportedMethods supportedMethods,
-		ServerCapabilities expected
-	) {
-		using var kestrel = new TestServer(
-			new WebHostBuilder()
-				.ConfigureServices(
-					services => services
-						.AddRouting()
-						.AddGrpc().Services
-						.AddSingleton(new FakeServerFeatures(supportedMethods))
-				)
-				.Configure(
-					app => app
-						.UseRouting()
-						.UseEndpoints(ep => ep.MapGrpcService<FakeServerFeatures>())
-				)
-		);
-
-		var sut = new GrpcServerCapabilitiesClient(new());
-
-		var actual =
-			await sut.GetAsync(
-				ChannelFactory
-					.CreateChannel(
-						new() {
-							CreateHttpMessageHandler = kestrel.CreateHandler
-						},
-						new DnsEndPoint("localhost", 80)
-					)
-					.CreateCallInvoker(),
-				default
-			);
-
-		Assert.Equal(expected, actual);
-	}
-
-	class FakeServerFeatures : ServerFeatures.ServerFeatures.ServerFeaturesBase {
-		readonly SupportedMethods _supportedMethods;
-
-		public FakeServerFeatures(SupportedMethods supportedMethods) => _supportedMethods = supportedMethods;
-
-		public override Task<SupportedMethods> GetSupportedMethods(Empty request, ServerCallContext context) => Task.FromResult(_supportedMethods);
-	}
-}
-#endif
+// #if NET
+// using System.Net;
+// using EventStore.Client.ServerFeatures;
+// using Grpc.Core;
+// using Microsoft.AspNetCore.Builder;
+// using Microsoft.AspNetCore.Hosting;
+// using Microsoft.AspNetCore.TestHost;
+// using Microsoft.Extensions.DependencyInjection;
+//
+// namespace EventStore.Client.Tests;
+//
+// public class GrpcServerCapabilitiesClientTests {
+// 	public static IEnumerable<object?[]> ExpectedResultsCases() {
+// 		yield return new object?[] { new SupportedMethods(), new ServerCapabilities() };
+// 		yield return new object?[] {
+// 			new SupportedMethods {
+// 				Methods = {
+// 					new SupportedMethod {
+// 						ServiceName = "event_store.client.streams.streams",
+// 						MethodName  = "batchappend"
+// 					}
+// 				}
+// 			},
+// 			new ServerCapabilities(true)
+// 		};
+//
+// 		yield return new object?[] {
+// 			new SupportedMethods {
+// 				Methods = {
+// 					new SupportedMethod {
+// 						ServiceName = "event_store.client.persistent_subscriptions.persistentsubscriptions",
+// 						MethodName  = "read",
+// 						Features = {
+// 							"all"
+// 						}
+// 					}
+// 				}
+// 			},
+// 			new ServerCapabilities(SupportsPersistentSubscriptionsToAll: true)
+// 		};
+//
+// 		yield return new object?[] {
+// 			new SupportedMethods {
+// 				Methods = {
+// 					new SupportedMethod {
+// 						ServiceName = "event_store.client.persistent_subscriptions.persistentsubscriptions",
+// 						MethodName  = "read"
+// 					}
+// 				}
+// 			},
+// 			new ServerCapabilities()
+// 		};
+// 	}
+//
+// 	[Theory]
+// 	[MemberData(nameof(ExpectedResultsCases))]
+// 	internal async Task GetAsyncReturnsExpectedResults(
+// 		SupportedMethods supportedMethods,
+// 		ServerCapabilities expected
+// 	) {
+// 		using var kestrel = new TestServer(
+// 			new WebHostBuilder()
+// 				.ConfigureServices(
+// 					services => services
+// 						.AddRouting()
+// 						.AddGrpc().Services
+// 						.AddSingleton(new FakeServerFeatures(supportedMethods))
+// 				)
+// 				.Configure(
+// 					app => app
+// 						.UseRouting()
+// 						.UseEndpoints(ep => ep.MapGrpcService<FakeServerFeatures>())
+// 				)
+// 		);
+//
+// 		var sut = new GrpcServerCapabilitiesClient(new());
+//
+// 		var actual =
+// 			await sut.GetAsync(
+// 				ChannelFactory
+// 					.CreateChannel(
+// 						new() {
+// 							CreateHttpMessageHandler = kestrel.CreateHandler
+// 						},
+// 						new DnsEndPoint("localhost", 80)
+// 					)
+// 					.CreateCallInvoker(),
+// 				default
+// 			);
+//
+// 		Assert.Equal(expected, actual);
+// 	}
+//
+// 	class FakeServerFeatures : ServerFeatures.ServerFeatures.ServerFeaturesBase {
+// 		readonly SupportedMethods _supportedMethods;
+//
+// 		public FakeServerFeatures(SupportedMethods supportedMethods) => _supportedMethods = supportedMethods;
+//
+// 		public override Task<SupportedMethods> GetSupportedMethods(Empty request, ServerCallContext context) => Task.FromResult(_supportedMethods);
+// 	}
+// }
+// #endif
